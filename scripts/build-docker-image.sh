adbf66948cdb9289df7d562bb61eaa1bcc93a30a
agoel-invn <ayush.goel@tdk.com>
2023-09-07 00:45:24 +0000

    Updated docker build shell script

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 1. Update docker-compose.yaml with ros topics
-ros2_topics_list=$(yq e '.iot.ros2_topics | join(",")' ../configurations/configuration.yaml)
+# ros2_topics_list=$(yq e '.iot.ros2_topics | join(",")' ../configurations/configuration.yaml)
 
-iot_topics_list=$(yq e '.iot.iot_topics | join(",")' ../configurations/configuration.yaml)
+# iot_topics_list=$(yq e '.iot.iot_topics | join(",")' ../configurations/configuration.yaml)
 
-yq e -i '.services.greengrass_bridge.command = "ros2 launch greengrass_bridge greengrass_bridge.launch.py ros_topics:=['"$ros2_topics_list"'] iot_topics:=['"$iot_topics_list"']"' ../docker/docker-compose.yaml
+# yq e -i '.services.greengrass_bridge.command = "ros2 launch greengrass_bridge greengrass_bridge.launch.py ros_topics:=['"$ros2_topics_list"'] iot_topics:=['"$iot_topics_list"']"' ../docker/docker-compose.yaml
 
-# cd ../docker/
-# sudo DOCKER_BUILDKIT=1 docker-compose build
+cd ../docker/
+sudo DOCKER_BUILDKIT=1 docker-compose build
